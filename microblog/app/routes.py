42f5069b4df1b476701f703327528a8eedb4a981
kavolkova <120653147+kavolkova@users.noreply.github.com>
2023-02-08 09:38:58 -0500

    chapter 2 step 2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hey squad!"
+    user = {'username': 'katerpillar'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
